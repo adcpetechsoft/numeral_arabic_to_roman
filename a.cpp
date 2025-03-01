758cb941aa94665c31e6da31f91b41a57d5a2c55
amdg <anvi_d@hotmail.com>
2025-03-01 14:42:48 +0800

    creation

---
+++ a.cpp
@@ -0,0 +1,24 @@
+#define STR_MAX 20
+
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+
+    int a;
+    int b;
+    char str01[STR_MAX];
+
+    if(argc==2)
+    {
+        
+
+    }else
+    {
+        printf("ERROR: No ARG Input...\n\n");
+    };
+
+
+    return 0;
+
+}
